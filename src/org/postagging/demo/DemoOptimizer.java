1d15c02e225103647b9a03236223ae5419169388
Asher Stern <astern7@gmail.com>
2014-11-07 18:17:00 +0200

    very minor

--- src/org/postagging/demo/DemoOptimizer.java
+++ src/org/postagging/demo/DemoOptimizer.java
@@ -42,60 +42,60 @@ public class DemoOptimizer
 		System.out.println("value = "+String.format("%-3.3f",optimizer.getValue()));
 	}
 	
-	private DerivableFunction createFunction()
-	{
-		// (x+2)^2
-		DerivableFunction function = new DerivableFunction()
-		{
-			@Override
-			public double value(double[] point)
-			{
-				return (point[0]+2.0)*(point[0]+2.0);
-			}
-			
-			@Override
-			public int size()
-			{
-				return 1;
-			}
-			
-			@Override
-			public double[] gradient(double[] point)
-			{
-				return new double[]{2.0*(point[0]+2.0)};
-			}
-		};
-		
-		return function;
-	}
-
-	
-	
 //	private DerivableFunction createFunction()
 //	{
-//		// (x_1-2)^2 + (2-x_2)^2 + x_1*x_2
+//		// (x+2)^2
 //		DerivableFunction function = new DerivableFunction()
 //		{
 //			@Override
 //			public double value(double[] point)
 //			{
-//				return (point[0]-2.0)*(point[0]-2.0)+(2.0-point[1])*(2.0-point[1])+point[0]*point[1];
+//				return (point[0]+2.0)*(point[0]+2.0);
 //			}
 //			
 //			@Override
 //			public int size()
 //			{
-//				return 2;
+//				return 1;
 //			}
 //			
 //			@Override
 //			public double[] gradient(double[] point)
 //			{
-//				return new double[]{2.0*(point[0]-2.0)+point[1], -2.0*(2.0-point[1])+point[0]};
+//				return new double[]{2.0*(point[0]+2.0)};
 //			}
 //		};
 //		
 //		return function;
 //	}
+
+	
+	
+	private DerivableFunction createFunction()
+	{
+		// (x_1-2)^2 + (2-x_2)^2 + x_1*x_2
+		DerivableFunction function = new DerivableFunction()
+		{
+			@Override
+			public double value(double[] point)
+			{
+				return (point[0]-2.0)*(point[0]-2.0)+(2.0-point[1])*(2.0-point[1])+point[0]*point[1];
+			}
+			
+			@Override
+			public int size()
+			{
+				return 2;
+			}
+			
+			@Override
+			public double[] gradient(double[] point)
+			{
+				return new double[]{2.0*(point[0]-2.0)+point[1], -2.0*(2.0-point[1])+point[0]};
+			}
+		};
+		
+		return function;
+	}
 
 }
