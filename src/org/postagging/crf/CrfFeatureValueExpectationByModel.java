897b7acdb9d87fe1d3e9dfd4fa2b03e9d787ee3f
Asher Stern <astern7@gmail.com>
2014-11-11 17:25:14 +0200

    Fix bug and improve efficiency in feature value expectation by model

--- src/org/postagging/crf/CrfFeatureValueExpectationByModel.java
+++ src/org/postagging/crf/CrfFeatureValueExpectationByModel.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Set;
 
 import org.apache.log4j.Logger;
-import org.postagging.crf.features.CrfFilteredFeature;
 import org.postagging.utilities.TaggedToken;
 
 import static org.postagging.crf.CrfUtilities.safeAdd;
@@ -54,43 +53,91 @@ public class CrfFeatureValueExpectationByModel<K, G>
 	private void addValueForSentence(List<? extends TaggedToken<K, G>> sentence)
 	{
 		K[] sentenceTokens = CrfUtilities.extractSentence(sentence);
-		logger.debug("Running forward backward...");
 		CrfForwardBackward<K,G> forwardBackward = new CrfForwardBackward<K,G>(model,sentenceTokens);
 		forwardBackward.calculateForwardAndBackward();
-		logger.debug("Running forward backward - done.");
 
-		logger.debug("Running loop for each feature...");
-		CrfFilteredFeature<K, G>[] filteredFeatures = model.getFeatures().getFilteredFeatures();
-		for (int featureIndex=0;featureIndex<filteredFeatures.length;++featureIndex)
+		final double normalizationFactor = forwardBackward.getCalculatedNormalizationFactor();
+		
+		for (int tokenIndex=0;tokenIndex<sentenceTokens.length;++tokenIndex)
 		{
-			CrfFeature<K, G> feature = filteredFeatures[featureIndex].getFeature();
+			Set<G> possiblePreviousTags = null;
+			if (tokenIndex==0) {possiblePreviousTags=Collections.singleton(null);}
+			else {possiblePreviousTags=model.getTags();}
 			
-			double sum = 0.0;
-			for (int sentenceIndex=0;sentenceIndex<sentenceTokens.length;++sentenceIndex)
+			for (G previousTag : possiblePreviousTags)
 			{
-				Set<G> possiblePreviousTags = null;
-				if (sentenceIndex==0) {possiblePreviousTags=Collections.singleton(null);}
-				else {possiblePreviousTags=model.getTags();}
-				for (G previousTag : possiblePreviousTags)
+				for (G currentTag : model.getTags())
 				{
-					for (G currentTokenTag : model.getTags())
+					Set<Integer> activeFeatures = CrfUtilities.getActiveFeatureIndexes(model.getFeatures(),sentenceTokens,tokenIndex,currentTag,previousTag);
+					for (int featureIndex : activeFeatures)
 					{
-						double featureValue = feature.value(sentenceTokens,sentenceIndex,currentTokenTag,previousTag);
-						double alpha_forward_previousValue = 1.0;
-						if (sentenceIndex>0)
+						double featureValue = 0.0;
+						if (model.getFeatures().getFilteredFeatures()[featureIndex].isWhenNotFilteredIsAlwaysOne())
 						{
-							alpha_forward_previousValue = forwardBackward.getAlpha_forward()[sentenceIndex-1].get(previousTag);
+							featureValue = 1.0;
 						}
-						double beta_backward_value = forwardBackward.getBeta_backward().get(sentenceIndex).get(currentTokenTag);
+						else
+						{
+							featureValue = model.getFeatures().getFilteredFeatures()[featureIndex].getFeature().value(sentenceTokens,tokenIndex,currentTag,previousTag);
+						}
 						
-						double probabilityUnderModel = alpha_forward_previousValue*featureValue*beta_backward_value;
-						sum = safeAdd(sum, probabilityUnderModel);
+						if (featureValue!=0.0)
+						{
+							// Calculate probabilityUnderModel
+							double alpha_forward_previousValue = 1.0;
+							if (tokenIndex>0)
+							{
+								alpha_forward_previousValue = forwardBackward.getAlpha_forward()[tokenIndex-1].get(previousTag);
+							}
+							double beta_backward_value = forwardBackward.getBeta_backward().get(tokenIndex).get(currentTag);
+
+							double psi_probabilityForGivenIndexAndTags = CrfUtilities.oneTokenFormula(model,sentenceTokens,tokenIndex,currentTag,previousTag);
+							
+							double probabilityUnderModel = alpha_forward_previousValue*psi_probabilityForGivenIndexAndTags*beta_backward_value;
+							
+							double addToExpectation = (featureValue*probabilityUnderModel)/normalizationFactor;
+							
+							featureValueExpectation[featureIndex] = safeAdd(featureValueExpectation[featureIndex], addToExpectation);
+						}
 					}
 				}
 			}
-			featureValueExpectation[featureIndex] = safeAdd(featureValueExpectation[featureIndex], sum/forwardBackward.getCalculatedNormalizationFactor());
 		}
-		logger.debug("Running loop for each feature - done.");
+		
+		
+//		logger.debug("Running loop for each feature...");
+//		CrfFilteredFeature<K, G>[] filteredFeatures = model.getFeatures().getFilteredFeatures();
+//		for (int featureIndex=0;featureIndex<filteredFeatures.length;++featureIndex)
+//		{
+//			CrfFeature<K, G> feature = filteredFeatures[featureIndex].getFeature();
+//			
+//			double sum = 0.0;
+//			for (int sentenceIndex=0;sentenceIndex<sentenceTokens.length;++sentenceIndex)
+//			{
+//				Set<G> possiblePreviousTags = null;
+//				if (sentenceIndex==0) {possiblePreviousTags=Collections.singleton(null);}
+//				else {possiblePreviousTags=model.getTags();}
+//				for (G previousTag : possiblePreviousTags)
+//				{
+//					for (G currentTokenTag : model.getTags())
+//					{
+//						double featureValue = feature.value(sentenceTokens,sentenceIndex,currentTokenTag,previousTag);
+//						double alpha_forward_previousValue = 1.0;
+//						if (sentenceIndex>0)
+//						{
+//							alpha_forward_previousValue = forwardBackward.getAlpha_forward()[sentenceIndex-1].get(previousTag);
+//						}
+//						double beta_backward_value = forwardBackward.getBeta_backward().get(sentenceIndex).get(currentTokenTag);
+//						
+//						// error?
+//						double probabilityUnderModel = alpha_forward_previousValue*featureValue*beta_backward_value;
+//						sum = safeAdd(sum, probabilityUnderModel);
+//					}
+//				}
+//			}
+//			featureValueExpectation[featureIndex] = safeAdd(featureValueExpectation[featureIndex], sum/forwardBackward.getCalculatedNormalizationFactor());
+//		}
+//		logger.debug("Running loop for each feature - done.");
 	}
 	
 	
@@ -101,5 +148,6 @@ public class CrfFeatureValueExpectationByModel<K, G>
 	
 	private double[] featureValueExpectation;
 	
+	@SuppressWarnings("unused")
 	private static final Logger logger = Logger.getLogger(CrfFeatureValueExpectationByModel.class);
 }
